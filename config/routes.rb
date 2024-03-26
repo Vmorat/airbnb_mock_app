c94f8dec85ac0df3d4b2d9d34df666a2c8f2b5d4
Victoria Vallejo <victoria.vallejosastrias@ieseg.fr>
2024-03-26 14:57:06 +0100

    created the login path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # Properties routes
   # Defines the root path route ("/")
+  get '/login', to: 'pages#login'
+  get '/details', to: 'pages#details'
   # root "articles#index"
 end
